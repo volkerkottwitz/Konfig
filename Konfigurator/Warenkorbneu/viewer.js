af56067cc369286413ec469aec727f3487541b81
volkerkottwitz <vkottwitz@web.de>
2025-07-14 18:14:52 +0200

    Add files via upload

--- Konfigurator/Warenkorbneu/viewer.js
+++ Konfigurator/Warenkorbneu/viewer.js
@@ -919,8 +919,25 @@ const pdfUrl = URL.createObjectURL(pdfBlob);
 
 const tab = window.open();
 tab.document.write(`
-  <title>Anfrage_EWE_${requestNumber}.pdf</title>
-  <iframe width="100%" height="100%" style="border:none;" src="${pdfUrl}"></iframe>
+  <html>
+    <head>
+      <title>Anfrage_EWE_${requestNumber}.pdf</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      <style>
+        html, body {
+          margin: 0; padding: 0; height: 100%; overflow: hidden;
+        }
+        iframe {
+          border: none;
+          width: 100vw;
+          height: 100vh;
+        }
+      </style>
+    </head>
+    <body>
+      <iframe src="${pdfUrl}"></iframe>
+    </body>
+  </html>
 `);
   };
   
